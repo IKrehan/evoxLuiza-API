629961b29979d1cd78b91b1b1a044d258d5b3fa2
IKrehan <irvigkrehan@hotmail.com>
2020-11-20 03:51:49 -0300

    Set express app and db connection

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: "postgres",
+    host: "localhost",
+    username: "postgres",
+    password: "info1590",
+    database: "evoxLuiza",
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
