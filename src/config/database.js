fbb18d0ca1a14d6a726b57836d843267bf94c8cf
IKrehan <irvigkrehan@hotmail.com>
2020-11-20 22:35:08 -0300

    dotenv and minor fixes

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
     developmentConfig: {
         dialect: "postgres",
-        host: "localhost",
-        username: "postgres",
-        password: "info1590",
-        database: "evoxLuiza",
+        host: process.env.DBCONFIG_HOST,
+        username: process.env.DBCONFIG_USER,
+        password: process.env.DBCONFIG_PASS,
+        database: process.env.DBCONFIG_DATABASE,
         define: {
             timestamps: true,
             underscored: true,
