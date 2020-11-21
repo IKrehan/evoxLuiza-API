7839ebde756c7d66b82508485070c94ec4c319b3
IKrehan <irvigkrehan@hotmail.com>
2020-11-20 21:58:48 -0300

    heroku DATABASE_URL

--- src/database/index.js
+++ src/database/index.js
@@ -6,7 +6,7 @@ const Product = require('../models/Product');
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-const connection = new Sequelize(isProduction ? productionConfig : developmentConfig);
+const connection = new Sequelize(isProduction ? process.env.DATABASE_URL : developmentConfig);
 
 Product.init(connection);
 
