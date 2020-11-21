787b5832b79ac099be3ec60479b4dcbe7d39b191
IKrehan <irvigkrehan@hotmail.com>
2020-11-20 21:39:10 -0300

    set dabatase for heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
 module.exports = {
     dialect: "postgres",
-    host: "localhost",
-    username: "postgres",
-    password: "info1590",
-    database: "evoxLuiza",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     define: {
         timestamps: true,
         underscored: true,
